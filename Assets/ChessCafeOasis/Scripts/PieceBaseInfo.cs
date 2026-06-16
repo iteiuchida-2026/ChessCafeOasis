565d70200bb12f294613abdbd8af1aaf5c7dfc34
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-16 11:53:59 +0900

    feat:PieceBaseInfoのenumを定義のみとなっていた状態を修正、色も追加する

--- Assets/ChessCafeOasis/Scripts/PieceBaseInfo.cs
+++ Assets/ChessCafeOasis/Scripts/PieceBaseInfo.cs
@@ -1,20 +1,30 @@
 using UnityEngine;
+public enum PieceType // 駒の種類を定義
+{
+    Pawn, // ポーン
+    Knight, // ナイト
+    Bishop, // ビショップ
+    Rook, // ルーク
+    Queen, // クイーン
+    King //キング
+}
+public enum PieceColor // 駒の色を定義
+{
+    White, // 白
+    Black // 黒
+}
 
 // チェス駒それぞれに持つ情報
 [CreateAssetMenu(fileName = "PieceBaseInfo", menuName = "Scriptable Objects/PieceBaseInfo")]
 public class PieceBaseInfo : ScriptableObject
 {
-    public string pieceName; // 駒の名前（a1ポーン等）
-    public enum pieceType // 駒の種類
-    {
-        Pawn, // ポーン
-        Knight, // ナイト
-        Bishop, // ビショップ
-        Rook, // ルーク
-        Queen, // クイーン
-        King //キング
-    }
-    public string PieceColor; // 駒の色（白、黒）
-    public int maxMoveSquares;　// 駒の最大移動可能マス数（ルークなら7）
+    public string pieceName; // 駒の名前（a2ポーン等）
+
+    public PieceType pieceType; // 駒の種類
+
+    public PieceColor pieceColor; // 駒の色
+
+    public int maxMoveSquares; // 駒の最大移動可能マス数（ルークなら7）
+
     public GameObject startingSquare; // 駒の初期配置マス（a1）をアタッチする
 }
