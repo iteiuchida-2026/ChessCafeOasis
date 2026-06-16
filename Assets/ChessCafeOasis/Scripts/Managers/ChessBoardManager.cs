b399c2802bf69d07d399f2b374823ae3e413b81c
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-16 22:05:10 +0900

    feat:ChessBoardManager.csのスクリプトを作成中

--- Assets/ChessCafeOasis/Scripts/Managers/ChessBoardManager.cs
+++ Assets/ChessCafeOasis/Scripts/Managers/ChessBoardManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+//////// スクリプトの説明：【チェス盤の各マスにどの駒が存在するか記録し管理する】////////
 
 public class ChessBoardManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private List<Piece> pieceList; // Pieceを格納するリストを宣言
+    [SerializeField] private List<TileController> tileControllers; // TileControllerを格納するリストを宣言
+    [SerializeField] private PieceFactory pieceFactory; // PieceFactoryの参照を取得するための変数を宣言
+    [SerializeField] private InputHandler inputHandler; // InputHandlerの参照を取得するための変数を宣言
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
