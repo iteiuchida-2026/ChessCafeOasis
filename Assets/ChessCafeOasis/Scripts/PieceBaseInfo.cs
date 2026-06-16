abf71a0361d6c83cd188a4e5d22b8d1e61388428
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-16 11:32:51 +0900

    feat:PieceBaseInfo.csのスクリプトを追加

--- Assets/ChessCafeOasis/Scripts/PieceBaseInfo.cs
+++ Assets/ChessCafeOasis/Scripts/PieceBaseInfo.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
+// チェス駒それぞれに持つ情報
 [CreateAssetMenu(fileName = "PieceBaseInfo", menuName = "Scriptable Objects/PieceBaseInfo")]
 public class PieceBaseInfo : ScriptableObject
 {
-    
+    public string pieceName; // 駒の名前（a1ポーン等）
+    public string pieceType; // 駒の種類（ポーン、ナイト）
+    public string PieceColor; // 駒の色（白、黒）
+    public int maxMoveSquares;　// 駒の最大移動可能マス数（ルークなら7）
+    public GameObject startingSquare; // 駒の初期配置マス（a1）をアタッチする
 }
