ad0b28d3c04edb573710025c358ae2f9921386d1
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-16 12:19:47 +0900

    feat:Piece.csのスクリプトを作成中

--- Assets/ChessCafeOasis/Scripts/Pieces/Piece.cs
+++ Assets/ChessCafeOasis/Scripts/Pieces/Piece.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("駒の基本情報を設定")]
+    [SerializeField] private PieceBaseInfo pieceBaseInfo;
+
+    private string pieceName { get; set; } // 駒の名前
+    private string pieceType { get; set; } // 駒の種類
+    private string pieceColor { get; set; } // 駒の色
+    private int maxMoveSquares { get; set; } // 駒の最大移動可能マス数
+    private GameObject startingSquare { get; set; } // 駒の初期配置マス
+
+
+    // スタート時にPieceBaseInfoから情報を取得しておく
+    private void Start()
     {
-        
+        pieceName = pieceBaseInfo.pieceName; // 駒の名前をPieceBaseInfoから取得
+        pieceType = pieceBaseInfo.pieceType.ToString(); // 駒の種類をPieceBaseInfoから取得
+        pieceColor = pieceBaseInfo.pieceColor.ToString(); // 駒の色をPieceBaseInfoから取得
+        maxMoveSquares = pieceBaseInfo.maxMoveSquares; // 駒の最大移動可能マス数をPieceBaseInfoから取得
+        startingSquare = pieceBaseInfo.startingSquare; // 駒の初期配置マスをPieceBaseInfoから取得
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
     }
+
+    // 移動する
+    public void Move()
+    {
+
+    }
+
+    // 駒を取る
+    public void Take()
+    {
+
+    }
+
+    // 駒が取られる（thisObject）
+    public void OnTaken()
+    {
+
+    }
+
 }
