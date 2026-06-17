8122199212c98e560989fd8f1f4b039a5e35b66f
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-17 12:10:51 +0900

    feat:Piece.csのスクリプトを追加
    Gemini相談結果の状態管理方法で追記：
    boolで特殊状態、enumで基本状態を管理

--- Assets/ChessCafeOasis/Scripts/Pieces/Piece.cs
+++ Assets/ChessCafeOasis/Scripts/Pieces/Piece.cs
@@ -15,6 +15,19 @@ public class Piece : MonoBehaviour
     // 駒の現在位置把握用の設定
     public string currentSquare { get; set; } // 駒の現在位置
 
+    // ルール判定用のフラグ
+    public bool HasMoved { get; private set; } = false; // キャスリング判定用
+    public bool IsPromoted { get; private set; } = false; // ポーンのプロモーション用
+
+    // オブジェクトの現在の状態管理用
+    public enum PieceStatus
+    {
+        Active,
+        Moving, // クリックして移動待機中
+        Destroyed
+    }
+
+    public PieceStatus currentStatus { get; private set; } = PieceStatus.Active;
 
     // スタート時にPieceBaseInfoから情報を取得しておく
     public virtual void Start()
@@ -23,27 +36,38 @@ public class Piece : MonoBehaviour
         pieceColor = pieceBaseInfo.pieceColor.ToString();
     }
 
-    private void Update()
-    {
 
-    }
 
     // 移動処理
     public virtual void Move()
     {
+        // 移動処理が入る予定
 
+        HasMoved = true;
     }
 
-    // 駒を取る処理
-    public virtual void Take()
+    public virtual void Promote()
     {
+        IsPromoted = true;
 
+        // 変更処理が入る予定
     }
+
+
+    //// 駒を取る処理（保留）
+    //public virtual void Take()
+    //{
+
+    //}
+
 
     // 駒が取られる処理（thisObject）
     public virtual void OnTaken()
     {
+        currentStatus = PieceStatus.Destroyed;
 
+        // 現時点では削除処理とするが、今後はチェス盤外へ移動する演出としたい
+        gameObject.SetActive(false);
     }
 
 }
