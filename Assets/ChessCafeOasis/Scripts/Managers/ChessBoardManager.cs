afb1585596546badadcae772f50fbbbf718c11b2
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-17 10:08:48 +0900

    docs:PieceFactryの名称をpieceManagerに変更。
    ・PieceManagerで3Dの駒オブジェクトの移動や削除まで担当する想定に変更。

--- Assets/ChessCafeOasis/Scripts/Managers/ChessBoardManager.cs
+++ Assets/ChessCafeOasis/Scripts/Managers/ChessBoardManager.cs
@@ -1,5 +1,5 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
 //////// スクリプトの説明：【チェス盤の各マスにどの駒が存在するか記録し管理する】////////
 
@@ -7,16 +7,16 @@ public class ChessBoardManager : MonoBehaviour
 {
     [SerializeField] private List<Piece> pieceList; // Pieceを格納するリストを宣言
     [SerializeField] private List<TileController> tileControllers; // TileControllerを格納するリストを宣言
-    [SerializeField] private PieceFactory pieceFactory; // PieceFactoryの参照を取得するための変数を宣言
+    [SerializeField] private PieceManager pieceFactory; // PieceFactoryの参照を取得するための変数を宣言
     [SerializeField] private InputHandler inputHandler; // InputHandlerの参照を取得するための変数を宣言
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
     }
 }
