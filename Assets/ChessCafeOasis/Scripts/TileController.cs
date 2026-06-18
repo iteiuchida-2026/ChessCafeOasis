3d18b75c9b0d28544d3e7fae4c82eb9da6c4ee13
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-18 10:15:45 +0900

    feat:TileController.csにマスの座標設定を追加

--- Assets/ChessCafeOasis/Scripts/TileController.cs
+++ Assets/ChessCafeOasis/Scripts/TileController.cs
@@ -1,10 +1,25 @@
 using UnityEngine;
 
-//////// スクリプトの説明：【3Dのチェス盤のマスにアタッチする。駒の移動時にマスを光らせる。】////////
-
+// ◆概要：3Dのチェス盤のマスの座標設定とマスのアクション
 public class TileController : MonoBehaviour
 {
-    // マスを光らせる
+    [Header("チェス盤の座標設定")]
+    [SerializeField] private Vector2Int boardIndex;
+
+    public Vector2Int BoardIndex => boardIndex;
+
+    // ▼棋譜用の文字列を返すプロパティ
+    public string AlgebraicNotation // AlgebraicNotationは要するにチェスの棋譜でe4等の表記（代数記法）
+    {
+        get
+        {
+            char file = (char)('a' + boardIndex.x); // 0→a,1→b
+            int rank = boardIndex.y + 1; // 0→1,1→2
+            return $"{file}{rank}";
+        }
+    }
+
+    // ▼マスを光らせるメソッド
     public void LightUpSquare()
     {
         // 後ほど追加する
