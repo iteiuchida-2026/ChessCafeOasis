d8c1f2acd39001997b288078bfab4aa04f8a53ea
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-18 15:42:50 +0900

    feat:PieceManager.csのPrefab登録を各色各駒で分けて設定に変更、駒の初期化について作成中

--- Assets/ChessCafeOasis/Scripts/Managers/PieceManager.cs
+++ Assets/ChessCafeOasis/Scripts/Managers/PieceManager.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class PieceManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] PiecePrefabs;
+    [Header("各駒のPrefab")]
+    [SerializeField] private GameObject WhitePawnPrefab;
+    [SerializeField] private GameObject WhiteKnightPrefab;
+    [SerializeField] private GameObject WhiteBishopPrefab;
+    [SerializeField] private GameObject WhiteRookPrefab;
+    [SerializeField] private GameObject WhiteQueenPrefab;
+    [SerializeField] private GameObject WhiteKingPrefab;
+    [SerializeField] private GameObject BlackPawnPrefab;
+    [SerializeField] private GameObject BlackKnightPrefab;
+    [SerializeField] private GameObject BlackBishopPrefab;
+    [SerializeField] private GameObject BlackRookPrefab;
+    [SerializeField] private GameObject BlackQueenPrefab;
+    [SerializeField] private GameObject BlackKingPrefab;
+
+    [Header("3Dチェス盤情報の取得先")]
     [SerializeField] private ChessBoardManager chessBoardManager;
 
-    private void Awake()
+    [Header("駒を生成する際のY軸（高さ）調整値")]
+    [SerializeField] private float SpawnY_Offset = 0.5f;
+
+    private void Start()
     {
-        Initialize3DBoard();
+        InitializeAllPiece();
     }
 
-    private void Initialize3DBoard()
+    private void InitializeAllPiece()
     {
-
+        GameObject a1 = chessBoardManager.GetPieceAtTileObjectsArray(0, 0);
     }
 }
