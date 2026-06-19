e72c9c6e937f0bdb392bf77e2d1294f447195c22
KazunoriUchida2026 <uni202601_01@telework.to>
2026-06-19 15:26:08 +0900

    feat:GameManager.csにシングルトン処理等を追加

--- Assets/ChessCafeOasis/Scripts/Managers/GameManager.cs
+++ Assets/ChessCafeOasis/Scripts/Managers/GameManager.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
+//////// スクリプトの説明：【各スクリプト間の情報の中継ハブ役、かつ指示役。】////////
+
+
+// ◆概要：
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("管理対象クラス")]
+    [SerializeField] private GameRoomSetUp gameRoomSetUp;
+    [SerializeField] private ChessRuleReferee chessRuleReferee;
+    [SerializeField] private RecordManager recordManager;
+    [SerializeField] private ChessBoardManager chessBoardManager;
+
+    public static GameManager Instance { get; private set; } // シングルトンのインスタンスを作成
+
+    private void Awake()
     {
-        
+        // シングルトンで永続化処理
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
